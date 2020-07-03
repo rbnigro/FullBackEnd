a52a85a5eb8e78b8478cefe24bf4b00aee17290b
helvecio <helvecio.m@hotmail.com>
2020-07-03 11:37:36 -0300

    Atualização do modelo de dados

--- src/main/java/br/com/linx/cardValidator/controller/BinController.java
+++ src/main/java/br/com/linx/cardValidator/controller/BinController.java
@@ -29,7 +29,7 @@ public class BinController {
         binTemplate = this.binServer.saveBin(binTemplate);
         return  new ResponseEntity<BinTemplate>(binTemplate, HttpStatus.OK);
     }
-
+//ajustes
 
 
 

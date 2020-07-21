fbbd721d205a2e496c53ff036b1882b633662083
helvecio <helvecio.m@hotmail.com>
2020-07-21 13:28:00 -0300

    Ajuste na estrutura

--- src/main/resources/db/migration/V1.1__SCRIPT_INICIAL.sql
+++ src/main/resources/db/migration/V1.1__SCRIPT_INICIAL.sql
@@ -1,78 +1,51 @@
 
 SET NAMES utf8 ;
 
--- Dumping database structure for AUTHENTICATOR_DEWA
--- CREATE DATABASE IF NOT EXISTS `cardvalidator` /*!40100 DEFAULT CHARACTER SET latin1 */;
--- USE `cardvalidator`;
 
--- Dumping structure for table AUTHENTICATOR_DEWA.access_monitor
-
-
-
-
-/* ------
- Schema
------- */
-/* CREATE SCHEMA cardvalidator DEFAULT CHARACTER SET latin1 COLLATE latin1_bin;
-
-/*
-------
-Tabelas
--------
--  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  */
+use cardvalidator;
 CREATE TABLE cardvalidator.teste(id_teste int(5),descr varchar(50));
 
-/*
-CREATE TABLE brand (
-  id_brand int(11) NOT NULL,
-  brand varchar(10) DEFAULT NULL,
-  description varchar(50) DEFAULT NULL,
-  created_at datetime NOT NULL);
+CREATE TABLE cardvalidator.brand (
+  id_brand bigint(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  brand varchar(10) ,
+  description varchar(50) ,
+  created_at datetime ,
+  UNIQUE KEY brand_UNIQUE (brand)
+);
+
+CREATE TABLE cardvalidator.bin (
+  id_bin bigint(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id_brand bigint(10),
+  bin bigint(10),
+  country varchar(10),
+  status varchar(10),
+  created_at datetime,
+  updated_at datetime,
+  UNIQUE KEY bin_UNIQUE (bin)
+);
+
+CREATE TABLE cardvalidator.bin_type (
+  id_type bigint(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  description varchar(50) ,
+  created_at datetime ,
+  UNIQUE KEY bin_UNIQUE (description)
+);
+
+ALTER TABLE cardvalidator.bin
+ADD INDEX fk_brand_idx (id_brand ASC);
+
+ALTER TABLE cardvalidator.bin
+ADD CONSTRAINT fk_brand
+  FOREIGN KEY (id_brand)
+  REFERENCES brand (id_brand)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
 
-CREATE TABLE bin (
-  id_bin int(11) NOT NULL,
-  id_brand int(11) NOT NULL,
-  bin int(11) DEFAULT NULL,
-  country varchar(10) DEFAULT NULL,
-  status varchar(10) NOT NULL,
-  created_at datetime NOT NULL,
-  updated_at datetime DEFAULT NULL);
-
-
-CREATE TABLE bin_type (
-  id_type int(11) NOT NULL,
-  description varchar(50) DEFAULT NULL,
-  created_at datetime NOT NULL);
-
-
-CREATE TABLE bin_bin_type (
-  binid_bin int(11) NOT NULL,
-  bin_typeid_type int(11) NOT NULL);
-
-/*
-------
-Dados
------*/
-/**
-INSERT INTO brand (id_brand, brand, description, created_at) VALUES(1, '506722', 'elo', '2020-07-02 00:00:00');
-INSERT INTO brand (id_brand, brand, description, created_at) VALUES(2, '485464', 'visa', '2020-07-02 00:00:00');
-
-
-INSERT INTO bin (id_bin, id_brand, bin, country, status, created_at, updated_at)
-VALUES (1, 1, 506744, 'brasil', 'ativo', '2020-07-02', null);
-INSERT INTO bin (id_bin, id_brand, bin, country, status, created_at, updated_at)
-VALUES (2, 1, 548799, 'brasil', 'ativo', '2020-07-02 00:00:00', '2020-07-03 00:00:00');
-INSERT INTO bin (id_bin, id_brand, bin, country, status, created_at, updated_at)
-VALUES (3, 2, 548966, 'brasil', 'inativo', '2020-07-02 00:00:00', '');
-
-
-INSERT INTO bin_type (id_type, description, created_at) VALUES (1,'debito','2020-07-02 00:00:00');
-INSERT INTO bin_type (id_type, description, created_at) VALUES (2,'credito','2020-07-02 00:00:00');
-INSERT INTO bin_type (id_type, description, created_at) VALUES (3,'refeicao','2020-07-02 00:00:00');
-
-
-INSERT INTO bin_bin_type (binid_bin, bin_typeid_type) VALUES (1, 1);
-INSERT INTO bin_bin_type (binid_bin, bin_typeid_type) VALUES (2, 2);
-
-*/
+CREATE TABLE cardvalidator.bin_bin_type (
+  binid_bin bigint(10) NOT NULL,
+  bin_typeid_type bigint(10) NOT NULL,
+  PRIMARY KEY (binid_bin,bin_typeid_type),
+  constraint FK_bin_bin_type foreign key (binid_bin) references bin(id_bin),
+  constraint FK_bin_bin_type2 foreign key (bin_typeid_type) references bin_type(id_type)
+  );
